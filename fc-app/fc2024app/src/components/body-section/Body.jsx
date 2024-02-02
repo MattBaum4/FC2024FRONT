e37e22e90c9187e9d23a84fb5a7ba36acc0a1aac
mattb <mattbaum44@gmail.com>
2024-02-01 20:29:29 -0600

    worked on styling and hero image sizing

--- fc-app/fc2024app/src/components/body-section/Body.jsx
+++ fc-app/fc2024app/src/components/body-section/Body.jsx
@@ -1,9 +1,16 @@
 import "./body.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const BodySection = () => {
   return (
     <>
-      <div className="body-pics"></div>
+      <div className="hero-section">
+         <img 
+         className="hero-img"
+         alt="poo" 
+         src="https://www.fuller.edu/wp-content/uploads/2023/04/Banner_4-1800x500-1.jpg"></img>
+      </div>
     </>
   );
 };
