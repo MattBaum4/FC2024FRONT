9b4047200b385fc33b8cc80db7a096507d49e7c7
mattb <mattbaum44@gmail.com>
2024-02-08 20:32:19 -0600

    added services page styled

--- fc-app/fc2024app/src/components/book-section/Book.jsx
+++ fc-app/fc2024app/src/components/book-section/Book.jsx
@@ -6,28 +6,77 @@ import CardBody from "react-bootstrap/esm/CardBody";
 import CardText from "react-bootstrap/esm/CardText";
 import sedanOut from "../../assets/sedanExterior.jpg"
 import sedanIn from "../../assets/blackInterior.jpg"
+import midsizeIn from "../../assets/midsizeInterior.jpg"
+import midsizeOut from "../../assets/midsizeExterior.jpg"
+import truckIn from "../../assets/truckInterior.jpg"
+import truckOut from "../../assets/truckExterior.jpg"
 const BookOnlineSection = () => {
     
     return ( 
     <>
         <div className="book-container">
             <div className="book-txt-section">
-                <h1>Book Online</h1>
+                <p>Our services</p>
             </div>
          <div className="card-container">
-            <Card className="card-actual">
+            <Card className="card-row">    
                 <CardBody>
-                    <CardText>Sedan Full Package</CardText>
+                    <CardText className="card-titles">Sedan Complete Detail</CardText>
                 <Card.Img className="card-imgs" alt="" variant="top" src={sedanOut}/>
-                <Button className="card-btns" variant="primary">Book</Button>
                 </CardBody>
+                <CardText className="card-pricing">this is the price</CardText>
+                <CardText className="card-time">time</CardText>
+                <Button className="card-btns" variant="primary">View Calendar</Button>
             </Card>
-            <Card className="card-actual">
+            <Card className="card-row">
                 <CardBody>
-                    <CardText>Sedan Full Package</CardText>
+                    <CardText className="card-titles">Sedan Interior Only</CardText>
                 <Card.Img className="card-imgs" alt="" variant="top" src={sedanIn}/>
-                <Button className="card-btns" variant="primary">Book</Button>
                 </CardBody>
+                <CardText className="card-pricing">this is the price</CardText>
+                <CardText className="card-time">time</CardText>
+                <Button className="card-btns" variant="primary">View Calendar</Button>
+            </Card>
+         </div>
+
+         <div className="card-container">
+         <Card className="card-row">
+                <CardBody>
+                    <CardText className="card-titles">Mid-size SUV Complete detail</CardText>
+                <Card.Img className="card-imgs" alt="" variant="top" src={midsizeOut}/>
+                </CardBody>
+                <CardText className="card-pricing">this is the price</CardText>
+                <CardText className="card-time">time</CardText>
+                <Button className="card-btns" variant="primary">View Calendar</Button>
+            </Card>
+         <Card className="card-row">
+                <CardBody>
+                    <CardText className="card-titles">Mid-size SUV Interior Only</CardText>
+                <Card.Img className="card-imgs" alt="" variant="top" src={midsizeIn}/>
+                </CardBody>
+                <CardText className="card-pricing">this is the price</CardText>
+                <CardText className="card-time">time</CardText>
+                <Button className="card-btns" variant="primary">View Calendar</Button>
+            </Card>
+         </div>
+         <div className="card-container">
+         <Card className="card-row">
+                <CardBody>
+                    <CardText className="card-titles">SUV/Truck Complete Detail</CardText>
+                <Card.Img className="card-imgs" alt="" variant="top" src={truckOut}/>
+                </CardBody>
+                <CardText className="card-pricing">this is the price</CardText>
+                <CardText className="card-time">time</CardText>
+                <Button className="card-btns" variant="primary">View Calendar</Button>
+            </Card>
+         <Card className="card-row">
+                <CardBody>
+                    <CardText className="card-titles">SUV/Truck Interior Only</CardText>
+                <Card.Img className="card-imgs" alt="" variant="top" src={truckIn}/>
+                </CardBody>
+                <CardText className="card-pricing">this is the price</CardText>
+                <CardText className="card-time">time</CardText>
+                <Button className="card-btns" variant="primary">View Calendar</Button>
             </Card>
          </div>
         </div>
