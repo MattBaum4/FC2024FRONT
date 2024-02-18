06f595597c048567a78fde416c94617680ef17bd
mattb <mattbaum44@gmail.com>
2024-02-17 19:34:41 -0600

    added a leaflet map to service area page

--- fc-app/fc2024app/src/components/contact-section/Contact.jsx
+++ fc-app/fc2024app/src/components/contact-section/Contact.jsx
@@ -1,17 +1,12 @@
-import Card from "react-bootstrap/Card";
 import "./contact.css"
-import CardHeader from "react-bootstrap/esm/CardHeader";
+
 
 const ContactSection = () => {
     return ( 
         <>
         <h2>Hello from Contact</h2>
         <div>
-            <Card>
-                <CardHeader>Contact Us!</CardHeader>
-                <p>heres a way to contact</p>
-                <p>heres a way to contact</p>
-            </Card>
+            
         </div>
         </>
      );
--- fc-app/fc2024app/src/components/serviceArea-section/ServiceArea.jsx
+++ fc-app/fc2024app/src/components/serviceArea-section/ServiceArea.jsx
@@ -1,8 +1,25 @@
+import "./servicearea.css"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css"
+
 const ServiceAreaSection = () => {
-    return ( 
-    <>
-    <h2>Hello from service area</h2>
-    </> );
-}
+   
+    return (
+      <>
+        <div className="map-container">
+          <MapContainer className="leaflet-map" center={[35.925, -86.868]} zoom={12} scrollWheelZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            
+          </MapContainer>
+        </div>
+        <p className="serv-txt">we proudly serve williamson county, mainly in close proximity to downtown franklin</p>
+      </>
+    );
+  }
  
-export default ServiceAreaSection;
+export default ServiceAreaSection; 
+
