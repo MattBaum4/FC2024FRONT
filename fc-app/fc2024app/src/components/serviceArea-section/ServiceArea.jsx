4262e0d86db813a969391bfc77716333808f69d0
mattb <mattbaum44@gmail.com>
2024-02-19 16:32:10 -0600

    fixed map and added dependecies for calendar

--- fc-app/fc2024app/src/components/serviceArea-section/ServiceArea.jsx
+++ fc-app/fc2024app/src/components/serviceArea-section/ServiceArea.jsx
@@ -8,7 +8,7 @@ const ServiceAreaSection = () => {
     return (
       <>
         <div className="map-container">
-          <MapContainer className="leaflet-map" center={[35.925, -86.868]} zoom={12} scrollWheelZoom={true}>
+          <MapContainer className="leaflet-map" center={[35.925, -86.868]} zoom={11} scrollWheelZoom={true}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
