f8a7e771a612205b645bea482701ebcbc767a514
mattb <mattbaum44@gmail.com>
2024-02-19 18:22:16 -0600

    added beginning stage of calendar

--- fc-app/fc2024app/src/components/calendar-section/Calendar.jsx
+++ fc-app/fc2024app/src/components/calendar-section/Calendar.jsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
-import "./calendar.css"
-
+import * as React from "react";
+import "./calendar.css";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const CalendarSection = () => {
-   
-    return ( 
-        <>
-      
-        </>
-     );
-}
- 
+  return (
+    <>
+      <div className="calendar-contain">
+        <FullCalendar
+        plugins={[dayGridPlugin]} 
+        initialView="dayGridMonth" 
+        />
+      </div>
+    </>
+  );
+};
+//maybe google form here instead 
 export default CalendarSection;
